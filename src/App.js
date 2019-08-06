b58df0dfd56d7b64ccd437d81608173fae8d63a0
Екатерина Гавренко <kategavua@gmail.com>
2019-08-06 16:07:20 +0100

    Replace TV show titles with covers #2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import cover from './about_time.jpg';
 import './App.css';
+import crown from './crown.jpg';
+import chernobyl from './chernobyl.jpg';
+import friends from './friends.jpg';
+import biglittlelies from './biglittlelies.jpg';
+import blackmirror from './blackmirror.jpg';
+import outlander from './outlander.jpg';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Codflix!</h1>
       <img src={cover} className="cover-photo" alt="cover-photo" />
+      <div className='series'>
+        <h2>My favorite series</h2>
+        <div>
+          <img src={crown} alt='crown'></img>
+          <img src={friends} alt='friends'></img>
+          <img src={chernobyl} alt='chernobyl'></img>
+        </div>
+        <div>
+          <img src={biglittlelies} alt='biglittlelies'></img>
+          <img src={blackmirror} alt='blackmirror'></img>
+          <img src={outlander} alt='outlander'></img>
+        </div>
+      </div>
     </div>
   );
 }
