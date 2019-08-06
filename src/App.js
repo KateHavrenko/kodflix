2deab7cb451591a8c611ec7133b30a0a5ed12e8d
Екатерина Гавренко <kategavua@gmail.com>
2019-08-06 14:01:05 +0100

    Add a  favourite movie cover

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import cover from './about_time.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Hello world!</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Codflix!</h1>
+      <img src={cover} className="cover-photo" alt="cover-photo" />
     </div>
   );
 }
