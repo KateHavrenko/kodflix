e6199f58c65db57f00a3119ed4dcbd70647b5ff3
Екатерина Гавренко <kategavua@gmail.com>
2019-09-02 13:19:09 +0100

    Push changes to heroku2

--- src/backend/app.js
+++ src/backend/app.js
@@ -3,7 +3,7 @@ const shows = require('./shows')
 const app = express()
 const port = process.env.PORT || 3001
 
-pp.use(express.static(path.join(__dirname, '../../build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
