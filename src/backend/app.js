c813bf0b7edb4d918562b2a12eda70325769bfac
Екатерина Гавренко <kategavua@gmail.com>
2019-09-02 15:04:07 +0100

    Push changes to heroku4

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,18 +1,29 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const port = process.env.PORT || 3001;
+// const shows = require('./shows');
+// app.get('/rest/shows', (req, res) => res.send(shows()));
+// app.listen(3001, () => console.log('Example app listening on port 3000!'));
+// app.use(express.static(path.join(__dirname, '../../build')));
+// app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+// });
+
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
-const shows = require('./shows');
-
-app.get('/rest/shows', (req, res) => {
-    res.send(shows())
-});
-
+const getShows = require('./shows');
 app.use(express.static(path.join(__dirname, '../../build')));
-
+// Handle React routing, return all requests to React app
+app.get('/rest/shows', (req, res) => res.send(getShows()));
+//app.listen(3001, () => console.log('Example app listening on port 3000!'));
+// Serve any static files
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+   res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
