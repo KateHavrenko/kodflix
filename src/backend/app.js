2dfc040059868ef40a57a906ef3be79fbe05b7de
Екатерина Гавренко <kategavua@gmail.com>
2019-09-02 13:42:35 +0100

    Push changes to heroku3

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const shows = require('./shows')
-const app = express()
-const port = process.env.PORT || 3001
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3001;
+const shows = require('./shows');
+
+app.get('/rest/shows', (req, res) => {
+    res.send(shows())
+});
 
 app.use(express.static(path.join(__dirname, '../../build')));
 
@@ -10,4 +15,4 @@ app.get('*', function (req, res) {
 });
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
